68115f53dd951886857d0b9a378c8abbe6e33ba3
maria <airamfdezs@gmailcom>
2022-02-28 21:01:42 +0100

    installations e initial config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+  .setTitle('TFG Api')
+  .setDescription('API to manage my e-commerce')
+  .setVersion('1.0')
+  .build()
+
+  const document = SwaggerModule.createDocument(app,config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
